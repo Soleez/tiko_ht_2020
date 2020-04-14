d854914a0bc2df63260bc8aa02b40e432c779194
ottot1 <60689161+ottot1@users.noreply.github.com>
2020-04-14 18:37:54 +0300

    T2: lisäyslomakkeelle lisätty uusia rivejä
    Nyt mahdollista kirjata 3 eri työkalua ja 3 eri työtuntia kerralla. Työtuntien tyypit on nyt kiinnitetty.

--- project/software/controllers/Add_rows.php
+++ project/software/controllers/Add_rows.php
@@ -42,61 +42,136 @@
 // Kun lisättävät tiedot annettu
 if (isset($_POST['formSubmit1']))
 {
+    // Valittujen tarvikkeiden id:t.
     $selected_tool1 = intval($_POST['tool1']);
-    $selected_work1 = intval($_POST['work1']);
-
+    $selected_tool2 = intval($_POST['tool2']);
+    $selected_tool3 = intval($_POST['tool3']);
+    
+    // Tarvikkeiden ja työtuntien määrät. 
     $amount_of_tools1 = intval($_POST['tool_amount1']);
+    $amount_of_tools2 = intval($_POST['tool_amount2']);
+    $amount_of_tools3 = intval($_POST['tool_amount3']);
     $amount_of_work1 = intval($_POST['work_amount1']);
+    $amount_of_work2 = intval($_POST['work_amount2']);
+    $amount_of_work3 = intval($_POST['work_amount3']);
 
+    // Alennusprosentit.
     $tool_sale1 = intval($_POST['tool_sale1']);
+    $tool_sale2 = intval($_POST['tool_sale2']);
+    $tool_sale3 = intval($_POST['tool_sale3']);
     $work_sale1 = intval($_POST['work_sale1']);
+    $work_sale2 = intval($_POST['work_sale2']);
+    $work_sale3 = intval($_POST['work_sale3']);
 
     $currentdate = pg_escape_string(date('Y-m-d'));
 
     // Tietojen tarkistus
-    $valid_info = $selected_contract != 0 && (($selected_tool1 != 0 && $amount_of_tools1 > 0) || ($selected_work1 != 0 && $amount_of_work1 > 0));
-
-    /*
-    var_dump($selected_tool1, $amount_of_tools1, $tool_sale1, $selected_contract);
-    var_dump($selected_work1, $amount_of_work1, $work_sale1, $selected_contract); die;
-    */
+    $valid_info = $selected_contract != 0 && (($selected_tool1 != 0 && $amount_of_tools1 > 0) || ($selected_tool2 != 0 && $amount_of_tools2 > 0) || ($selected_tool3 != 0 && $amount_of_tools3 > 0) 
+    || $amount_of_work1 > 0 || $amount_of_work2 > 0 || $amount_of_work3 > 0);
 
     if ($valid_info) {
         if ($selected_tool1 != 0 && $amount_of_tools1 > 0) {
 
-            $query1 = "INSERT INTO Sold_tool VALUES (DEFAULT, '$selected_tool1', '$amount_of_tools1', '$selected_contract', '$currentdate', '$tool_sale1');";
-            $update1 = pg_query($query1);
+            $t_query1 = "INSERT INTO Sold_tool VALUES (DEFAULT, '$selected_tool1', '$amount_of_tools1', '$selected_contract', '$currentdate', '$tool_sale1');";
+            $t_update1 = pg_query($t_query1);
 
             // asetetaan msg1-muuttuja lisäämisen onnistumisen mukaan
             // lisätään virheilmoitukseen myös virheen syy (pg_last_error)
     
-            if ($update1 && (pg_affected_rows($update1) > 0)) {
-                $msg1 = 'Tarvikkeet kirjattu!';
+            if ($t_update1 && (pg_affected_rows($t_update1) > 0)) {
+                $t_msg1 = 'Tarvike kirjattu!';
             }
             else {
-                $msg1 = 'Tarvikkeita ei lisätty: ' . pg_last_error();
+                $t_msg1 = 'Tarvikkeita ei lisätty: ' . pg_last_error();
             }
         }
 
-        if ($selected_work1 != 0 && $amount_of_work1 > 0) {
-            $query2 = "INSERT INTO Billable_hour VALUES (DEFAULT, '$selected_work1', '$selected_contract', '$currentdate', '$amount_of_work1', '$work_sale1');";
-            $update2 = pg_query($query2);
+        if ($selected_tool2 != 0 && $amount_of_tools2 > 0) {
+
+            $t_query2 = "INSERT INTO Sold_tool VALUES (DEFAULT, '$selected_tool2', '$amount_of_tools2', '$selected_contract', '$currentdate', '$tool_sale2');";
+            $t_update2 = pg_query($t_query2);
+
+            // asetetaan msg1-muuttuja lisäämisen onnistumisen mukaan
+            // lisätään virheilmoitukseen myös virheen syy (pg_last_error)
+    
+            if ($t_update2 && (pg_affected_rows($t_update2) > 0)) {
+                $t_msg2 = 'Tarvike kirjattu!';
+            }
+            else {
+                $t_msg2 = 'Tarvikkeita ei lisätty: ' . pg_last_error();
+            }
+        }
+
+        if ($selected_tool3 != 0 && $amount_of_tools3 > 0) {
+
+            $t_query3 = "INSERT INTO Sold_tool VALUES (DEFAULT, '$selected_tool3', '$amount_of_tools3', '$selected_contract', '$currentdate', '$tool_sale3');";
+            $t_update3 = pg_query($t_query3);
+
+            // asetetaan msg1-muuttuja lisäämisen onnistumisen mukaan
+            // lisätään virheilmoitukseen myös virheen syy (pg_last_error)
+    
+            if ($t_update3 && (pg_affected_rows($t_update3) > 0)) {
+                $t_msg3 = 'Tarvike kirjattu!';
+            }
+            else {
+                $t_msg3 = 'Tarvikkeita ei lisätty: ' . pg_last_error();
+            }
+        }
+
+        // Work_type_id 1 = suunnittelu, 2 = asennustyö, 3 = aputyö
+        
+        if ($amount_of_work1 > 0) {
+            $w_query1 = "INSERT INTO Billable_hour VALUES (DEFAULT, 1, '$selected_contract', '$currentdate', '$amount_of_work1', '$work_sale1');";
+            $w_update1 = pg_query($w_query1);
             
             // asetetaan msg2-muuttuja lisäämisen onnistumisen mukaan
             // lisätään virheilmoitukseen myös virheen syy (pg_last_error)
     
-            if ($update2 && (pg_affected_rows($update2) > 0)) {
-                $msg2 = 'Työtunnit kirjattu!';
+            if ($w_update1 && (pg_affected_rows($w_update1) > 0)) {
+                $w_msg1 = 'Työtunnit kirjattu!';
             }
             else {
-                $msg2 = 'Työtunteja ei lisätty: ' . pg_last_error();
+                $w_msg1 = 'Työtunteja ei lisätty: ' . pg_last_error();
+            }
+        }
+
+        if ($amount_of_work2 > 0) {
+            $w_query2 = "INSERT INTO Billable_hour VALUES (DEFAULT, 2, '$selected_contract', '$currentdate', '$amount_of_work2', '$work_sale2');";
+            $w_update2 = pg_query($w_query2);
+            
+            // asetetaan msg2-muuttuja lisäämisen onnistumisen mukaan
+            // lisätään virheilmoitukseen myös virheen syy (pg_last_error)
+    
+            if ($w_update2 && (pg_affected_rows($w_update2) > 0)) {
+                $w_msg2 = 'Työtunnit kirjattu!';
+            }
+            else {
+                $w_msg2 = 'Työtunteja ei lisätty: ' . pg_last_error();
+            }
+        }
+
+        if ($amount_of_work3 > 0) {
+            $w_query3 = "INSERT INTO Billable_hour VALUES (DEFAULT, 3, '$selected_contract', '$currentdate', '$amount_of_work3', '$work_sale3');";
+            $w_update3 = pg_query($w_query3);
+            
+            // asetetaan msg2-muuttuja lisäämisen onnistumisen mukaan
+            // lisätään virheilmoitukseen myös virheen syy (pg_last_error)
+    
+            if ($w_update3 && (pg_affected_rows($w_update3) > 0)) {
+                $w_msg3 = 'Työtunnit kirjattu!';
+            }
+            else {
+                $w_msg3 = 'Työtunteja ei lisätty: ' . pg_last_error();
             }
         }
 
     }
+
+    // Virheentarkastelu ilmoittaa virheestä vain kun yhtään riviä ei onnistuta lisäämään.
+
     else {
         if ($selected_contract == 0) {
-            $msg3 = 'Asiakasta ei ole valittu!';
+            $err_msg = 'Asiakasta ei ole valittu!';
         }
         /* else if ($selected_tool == 0) {
             $viesti = 'Valitse tarvike!';
@@ -105,7 +180,7 @@ if (isset($_POST['formSubmit1']))
             $viesti = 'Lisättävien tarvikkeiden määrän oltava enemmän kuin 0.';
         } */
         else {
-            $msg3 = 'Annetut tiedot puutteelliset - tarkista, ole hyvä!';
+            $err_msg = 'Annetut tiedot puutteelliset - tarkista, ole hyvä!';
         }
     }
 }
--- project/software/views/add_rows.php
+++ project/software/views/add_rows.php
@@ -4,8 +4,8 @@
 <html>
 
   <body>
-    <h2>Lisää tarvikkeita ja työtunteja työkohteelle</h2>
-    <div>Kirjataan tälle päivälle <?php echo date("Y-m-d"); ?></div>
+    <h2>Lisää tarvikkeita ja työtunteja sopimukselle</h2>
+    <div>Kirjataan tälle päivälle <?php echo date("Y-m-d"); ?>.</div>
 
     <form method="POST" action="add_rows.php">
       <div>
@@ -35,6 +35,40 @@
               <td><input type="number" name="tool_amount1" value="" min="0" /></td>
               <td><input type="number" name="tool_sale1" value="0" min="0" max="100" /></td>
             </tr>
+            <tr>
+              <td>
+                <select id="tool2" name="tool2">
+                  <option value="" disable selected hidden>Valitse</option>
+                  <?php 
+                  // Asetetaan kyselyn arvot rivi kerrallaan optioneiksi
+                  for ($row = 0; $row < count($tools); $row++ ) {
+                    ?>
+                    <option value="<?php echo $tools[$row]['tool_id'];?>"><?php echo $tool_name = $tools[$row]['tool_name']; ?>, <?php echo $tool_unit = $tools[$row]['unit']; ?></option>
+                    <?php
+                  }
+                  ?>
+                </select>
+              </td>
+              <td><input type="number" name="tool_amount2" value="" min="0" /></td>
+              <td><input type="number" name="tool_sale2" value="0" min="0" max="100" /></td>
+            </tr>
+            <tr>
+              <td>
+                <select id="tool3" name="tool3">
+                  <option value="" disable selected hidden>Valitse</option>
+                  <?php 
+                  // Asetetaan kyselyn arvot rivi kerrallaan optioneiksi
+                  for ($row = 0; $row < count($tools); $row++ ) {
+                    ?>
+                    <option value="<?php echo $tools[$row]['tool_id'];?>"><?php echo $tool_name = $tools[$row]['tool_name']; ?>, <?php echo $tool_unit = $tools[$row]['unit']; ?></option>
+                    <?php
+                  }
+                  ?>
+                </select>
+              </td>
+              <td><input type="number" name="tool_amount3" value="" min="0" /></td>
+              <td><input type="number" name="tool_sale3" value="0" min="0" max="100" /></td>
+            </tr>
           </tbody>
         </table>
       </div>
@@ -51,29 +85,47 @@
           <tbody>
             <tr>
               <td>
-                <select id="work1" name="work1">
-                  <option value="" disable selected hidden>Valitse</option>
-                  <?php 
-                    for ($row = 0; $row < count($worktypes); $row++ ) {
-                    ?>
-                    <option value="<?php echo $worktypes[$row]['work_type_id'];?>"><?php echo $work_type_name = $worktypes[$row]['work_type_name']; ?></option>
-                    <?php 
-                    }
-                  ?>
-                </select>
+                <?php 
+                  // work_type_id = 1: suunnittelu 
+                  echo $worktypes[0]['work_type_name'];
+                ?>
               </td>
               <td><input type="number" name="work_amount1" value="" min="0" /></td>
               <td><input type="number" name="work_sale1" value="0" min="0" max="100" /></td>
+            </tr>
+            <tr>
+              <td>
+                <?php
+                  // work_type_id = 2: asennustyö 
+                  echo $worktypes[1]['work_type_name'];
+                ?>
+              </td>
+              <td><input type="number" name="work_amount2" value="" min="0" /></td>
+              <td><input type="number" name="work_sale2" value="0" min="0" max="100" /></td>
+            </tr>
+            <tr>
+              <td>
+                <?php
+                  // work_type_id = 3: aputyö 
+                  echo $worktypes[2]['work_type_name'];
+                ?>
+              </td>
+              <td><input type="number" name="work_amount3" value="" min="0" /></td>
+              <td><input type="number" name="work_sale3" value="0" min="0" max="100" /></td>
             </tr>
           </tbody>
         </table>
       </div>
       
-      <input type="submit" name="formSubmit1" value="Kirjaa tarvikkeet/työtunnit työkohteeseen"/>
+      <input type="submit" name="formSubmit1" value="Kirjaa tarvikkeet/työtunnit sopimukselle"/>
 
-      <?php if (isset($msg1)) echo '<p>'.$msg1.'</p>'; ?>
-      <?php if (isset($msg2)) echo '<p>'.$msg2.'</p>'; ?>
-      <?php if (isset($msg3)) echo '<p>'.$msg3.'</p>'; ?>
+      <?php if (isset($t_msg1)) echo '<p>'.$t_msg1.'</p>'; ?>
+      <?php if (isset($t_msg2)) echo '<p>'.$t_msg2.'</p>'; ?>
+      <?php if (isset($t_msg3)) echo '<p>'.$t_msg3.'</p>'; ?>
+      <?php if (isset($w_msg1)) echo '<p>'.$w_msg1.'</p>'; ?>
+      <?php if (isset($w_msg2)) echo '<p>'.$w_msg2.'</p>'; ?>
+      <?php if (isset($w_msg3)) echo '<p>'.$w_msg3.'</p>'; ?>
+      <?php if (isset($err_msg)) echo '<p>'.$err_msg.'</p>'; ?>
     </form>
   </body>
 </html>
