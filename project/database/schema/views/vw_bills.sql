adbe873fd791a1abdc32fd5cf83d34c7682118d1
Jani <jani_makela.s@hotmail.com>
2020-04-19 18:03:02 +0300

    Siirretty viewit omiin tiedostoihin ja tety korjaus vw_bills:iin.

---
+++ project/database/schema/views/vw_bills.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE VIEW vw_bills
+AS
+    SELECT Contractor.Contractor_id, 
+    Customer.customer_name, 
+    Customer.customer_id,
+    Project.Project_id,
+    Project.Project_name,
+    Project.Project_address,
+    Contract.contract_id, 
+    Contract.amount_of_payments,
+    Contract_type.contract_type_name,
+    Bill.bill_id,
+    Bill.total_sum,
+    Bill.billing_address,
+    Bill_status.bill_status_name,
+    Bill_type.bill_type_name
+    FROM bill
+    LEFT OUTER JOIN contract 
+        ON contract.contract_id = Bill.contract_id
+    LEFT OUTER JOIN project
+        ON project.project_id = contract.project_id
+    LEFT OUTER JOIN customer
+        ON customer.customer_id = project.customer_id
+    LEFT OUTER JOIN contractor
+        ON contractor.contractor_id = customer.contractor_id
+    LEFT OUTER JOIN bill_status
+        ON bill_status.bill_status_id = bill.bill_status_id
+    LEFT OUTER JOIN bill_type
+        ON bill_type.bill_type_id = bill.bill_type_id
+    LEFT OUTER JOIN contract_type
+        ON contract_type.contract_type_id = contract.contract_type_id;
---
+++ project/database/schema/views/vw_hours.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE VIEW vw_hours
+AS
+    SELECT Contractor.Contractor_id, 
+    Customer.customer_name, 
+    Customer.customer_id, 
+    Project.Project_id,
+    Project.Project_name,
+    Contract.contract_id, 
+    Billable_hour.billable_hour_id,
+    Billable_hour.quantity, 
+    Billable_hour.date_added,
+    CAST (Billable_hour.sale_percentage AS NUMERIC(10,2)), 
+    Work_type.work_type_name, 
+    Work_type.hourly_rate, 
+    CAST (Vat_type.vat_rate AS NUMERIC(10,2)),
+    CAST (Billable_hour.quantity * Work_type.hourly_rate AS NUMERIC(10,2)) AS total_before_sale, --alkuperäinen hinta veron kanssa ennen alennusta
+    CAST ((Billable_hour.quantity * Work_type.hourly_rate * (Vat_type.vat_rate/100.00)) AS NUMERIC(10,2)) AS tax_only,   --alvin määrä
+    CAST ((Billable_hour.quantity * Work_type.hourly_rate * ((100.00 - Vat_type.vat_rate)/100.00) * ((100.00 - Billable_hour.sale_percentage)/100.00)) AS NUMERIC(10,2)) AS price_wo_tax_w_sale,  --ilman alvia, alennus mukana
+    CAST ((Billable_hour.quantity * Work_type.hourly_rate * (Vat_type.vat_rate/100.00)) AS NUMERIC(10,2)) + CAST ((Billable_hour.quantity * Work_type.hourly_rate * ((100.00 - Vat_type.vat_rate)/100.00) * ((100.00 - Billable_hour.sale_percentage)/100.00)) AS NUMERIC(10,2)) AS total_sum
+    FROM ((((((Contractor JOIN Customer ON Contractor.contractor_id = Customer.contractor_id)
+        JOIN Project ON Customer.customer_id = Project.customer_id)
+        JOIN Contract ON Project.project_id = Contract.project_id)
+        JOIN Billable_hour ON Contract.contract_id = Billable_hour.contract_id)
+        JOIN Work_type ON Billable_hour.work_type_id = Work_type.work_type_id)
+        JOIN Vat_type ON Vat_type.vat_type_id = Work_type.vat_type_id);
