9a059f7fad97e910cff15c39ee600c5386f814ca
Jani <jani_makela.s@hotmail.com>
2020-04-20 12:38:04 +0300

    test

--- project/database/schema/functions/toolsum_wo_discount_function.sql
+++ project/database/schema/functions/toolsum_wo_discount_function.sql
@@ -4,13 +4,14 @@ AS $suorita$
 BEGIN
     return(
     
-    select *, round(sum(quantity*tool_selling_price),2)
+    select * -- round(sum(quantity*tool_selling_price),2)
     from Sold_tool 
         join Tool on Sold_tool.tool_id = Tool.tool_id 
         join Vat_type on Tool.vat_type_id=Vat_type.vat_type_id
         LEFT OUTER JOIN bill on bill.contract_id = sold_tool.contract_id
-    where Sold_tool.contract_id = c_id AND date_added >);
-
+    where Sold_tool.contract_id = 5 
+        AND ((Sold_tool.date_added BETWEEN bill.date_added AND bill.bill_sending_date)
+        OR (Sold_tool.date_added BETWEEN bill.date_added AND CURRENT_DATE));
 END;
 $suorita$
 language plpgsql;
